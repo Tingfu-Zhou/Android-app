63dbcdafde9ee5b568955e7ab874760c4ecce706
Tingfu-Zhou <782234324@qq.com>
2026-05-25 15:55:03 +0800

    version 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.helloworld"
         minSdk = 26
         targetSdk = 35
-        versionCode = 112
-        versionName = "1.1.2"
+        versionCode = 113
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
